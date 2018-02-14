66255f5309a3f61a07e33faf113494130283b692
efraespada <efraespada@gmail.com>
2018-02-14 21:48:41 +0100

    ...

--- library/src/main/cpp/malacaton-lib.cpp
+++ library/src/main/cpp/malacaton-lib.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 extern "C"
-JNIEXPORT jstring JNICALL Java_com_stringcare_library_SC_getKey(JNIEnv *env, jobject) {
+JNIEXPORT jstring JNICALL Java_com_stringcare_library_SC_getKey(JNIEnv * env, jclass, jobject) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
