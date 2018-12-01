109524c9e90fd7af191a3c9f8513e3fb8f7aaaaa
Efraín Espada <efrainespada@Efrains-Mac-mini.local>
2018-12-01 19:28:44 +0100

    jclass jobject

--- library/src/main/cpp/native-lib.cpp
+++ library/src/main/cpp/native-lib.cpp
@@ -4,7 +4,7 @@
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_stringcare_library_SC_sign(
         JNIEnv *env,
-        jobject /* this */,
+        jclass /* this */,
         jstring key) {
     std::string hello = "Hello from C++";
     // return env->NewStringUTF(hello.c_str());
