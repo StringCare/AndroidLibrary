174b17844a44e33073bf014fd8541a4e82265a50
Efraín Espada <efrainespada@Efrains-Mac-mini.local>
2018-12-02 23:53:43 +0100

    ...

--- library/src/main/java/com/stringcare/library/SC.java
+++ library/src/main/java/com/stringcare/library/SC.java
@@ -49,8 +49,7 @@ public class SC {
     }
 
     /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
+     * Resigns given key
      */
     public static native String sign(String key);
 
